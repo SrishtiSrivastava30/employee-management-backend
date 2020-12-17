c788d5e545f4340ed22ad796e7fe7490c4678641
srishti <srivastavasrishti230@gmail.com>
2020-12-17 20:19:13 +0530

    all admin routes created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 // const {request} = require('http');
 const {request, response} = require('express');
 var db = mysql.createConnection({
@@ -9,6 +10,7 @@ var db = mysql.createConnection({
     database : 'company_db'
   });
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.listen('3000', () => {
@@ -18,10 +20,17 @@ db.connect(err => {
     if (err) throw err
     console.log('DB connected');
 })
+app.post('/login', (request, response) => {
+    let email = request.body.email;
+    let password = request.body.password;
+    if(email === "admin@gmail.com" && password === "cheems420"){
+        response.status(200).json({token: "59tyg2ohwbevoil3tu0479ow8pu9fejnb74w80pu9fjvn4ow8pguj"});
+    }
+    else{
+        response.status(401).json({token: "Unauthorized"})
+    }
+})
 app.post('/adddepartment', (request, response) => {
-    // const id = request.body.id;
-    // const name = request.body.name;
-    // const hod = request.body.hod;
     const {body: {id, name, hod}} = request;
     let emp = {DEPT_ID: id, DEPT_NAME: name, HEAD_OF_DEPARTMENTS: hod}
     let sql = 'INSERT INTO departments SET ?';
@@ -60,12 +69,45 @@ app.post('/deleteemployee', (request, response) => {
         }
         else{
             // console.log(result);
-            response.status(201).json({message: "Successfully deleted an employee record"});
+            db.query(`DELETE FROM payroll WHERE EMP_ID = ${emp_id}`, (err, res => {
+                if(err){
+                    response.status(500).json({error: 'Something went wrong'})
+                    console.log('error => ' + err);
+                }
+                else{
+                    response.status(201).json({message: "Successfully deleted an employee record and payroll"});
+                }
+            }))
         }
     });
 });
+app.post('/deleteDepartment', (request, response) => {
+    const {body: {dept_id}} = request;
+    db.query(`DELETE FROM departments WHERE DEPT_ID = ${dept_id}`, (err, result) => {
+        if (err){
+            response.status(500).json({error: 'Something went wrong'})
+            console.log('error => ' + err);
+        }
+        else{
+            response.status(201).json({message: "Successfully deleted a department"});
+        }
+    }
+    )
+});
 app.get('/empdetails', (request, response) => {
     db.query('SELECT * FROM employee', (err, result) => {
+        if (err){
+            response.status(500).json({error: 'Something went wrong'})
+            console.log('error => ' + err);
+        }
+        else{
+            // console.log(result);
+            response.status(201).json({message: result});
+        }
+    });
+});
+app.get('/depdetails', (request, response) => {
+    db.query('SELECT * FROM departments', (err, result) => {
         if (err){
             response.status(500).json({error: 'Something went wrong'})
             console.log('error => ' + err);
